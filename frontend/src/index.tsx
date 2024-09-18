b454487937e0e63ee3042486d3c2b41759ce3b6d
Desarrollo IA Punto <desarrollo@iapunto.com>
2024-09-17 21:10:59 -0500

    Integración Android

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import HomeScreen from './screens/HomeScreen';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HomeScreen);
