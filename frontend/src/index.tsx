8d8b6e524592bb461c84eb8aaa72801e30cda37b
Desarrollo IA Punto <desarrollo@iapunto.com>
2024-09-18 10:47:44 -0500

    Cambio de plantilla de sistema a EXPO

--- frontend/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => HomeScreen);
